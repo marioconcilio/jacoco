a7f2ab228a02c4a53f7b6f0b52df5147280459b4
Henrique Lemos Ribeiro <henriquelemos0@mgmail.com>
2018-04-16 10:29:40 -0300

    Add script to copy jars to jaguar

---
+++ prepare-jaguar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ./org.jacoco.agent/target/classes/jacocoagent.jar ../jaguar/br.usp.each.saeg.jaguar.plugin/lib/jacocoagent.jar 
+cp ./org.jacoco.core/target/org.jacoco.core-0.7.6-SNAPSHOT.jar ../jaguar/br.usp.each.saeg.jaguar.core/lib/org.jacoco.core-0.7.6.jar
